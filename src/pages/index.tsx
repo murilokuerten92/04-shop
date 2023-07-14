acd1803c9b88ddfedbaf61883a8b9a4219557530
murilokuerten92 <murilokuerten92@gmail.com>
2023-07-14 09:23:04 -0300

    feat: creating home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,21 @@
+import { HomeContainer, Product } from '@/styles/pages/home'
 import { styled } from '../styles'
-
-const Button = styled('button', {
-  backgroundColor: "$green500",
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px'
-})
+import Image from 'next/image'
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
+import camiseta3 from '../assets/camisetas/3.png'
 
 export default function Home() {
   return (
-    <Button>
-      test
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt='' />
+
+
+        <footer>
+
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
