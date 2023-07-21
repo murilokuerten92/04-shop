1a941fe751ed88a4f5e9f7ac4df9baf697cccfee
murilokuerten92 <murilokuerten92@gmail.com>
2023-07-21 13:03:33 -0300

    feat: adding Link to navigate more performance

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import { useKeenSlider } from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css'
 import { stripe } from '@/lib/stripe'
 import Stripe from 'stripe'
+import Link from 'next/link'
 
 interface HomeProps {
   products: {
@@ -29,17 +30,20 @@ export default function Home({ products }: HomeProps) {
   return (
     <HomeContainer ref={sliderRef} className='keen-slider'>
       {products.map(product => (
-        <Product className='keen-slider__slide' key={product.id}>
-          <Image src={product.imageUrl} width={520} height={480} alt='' />
+        <Link key={product.id} href={`/product/${product.id}`}>
+          <Product className='keen-slider__slide' >
+            <Image src={product.imageUrl} width={520} height={480} alt='' />
 
-          <footer>
-            <strong>{product.name}</strong>
-            <span>{product.price}</span>
-          </footer>
-        </Product>
-      ))}
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        </Link>
+      ))
+      }
 
-    </HomeContainer>
+    </HomeContainer >
   )
 }
 
