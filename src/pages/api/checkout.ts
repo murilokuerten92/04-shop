9f3deb5e18e00bf7f20633ef3d6c61d2440cb198
murilokuerten92 <murilokuerten92@gmail.com>
2023-07-25 09:29:40 -0300

    chore: creating redirect to checkout from buy button

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -2,10 +2,18 @@ import { stripe } from '@/lib/stripe';
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const priceId = 'price_1NViHUIpfc6OmuuywEkTqZ1u';
+    const { priceId } = req.body;
 
     const successUrl = `${process.env.NEXT_URL}/success`;
     const cancelUrl = `${process.env.NEXT_URL}/`;
+
+    if (req.method !== 'POST') {
+        return res.status(405).json({ error: 'Method not allowed' })
+    }
+
+    if (!priceId) {
+        return res.status(400).json({ error: 'Price not found' })
+    }
 
     const checkoutSession = await stripe.checkout.sessions.create({
         success_url: successUrl,
