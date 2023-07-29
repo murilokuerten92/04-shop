021ff672aec70d00c5cd42e75f36b75fea962777
murilokuerten92 <murilokuerten92@gmail.com>
2023-07-29 14:38:49 -0300

    feat: creating SEO to pages

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -5,6 +5,7 @@ import Stripe from "stripe";
 import Image from 'next/image'
 import axios from "axios";
 import { useState } from "react";
+import Head from "next/head";
 
 interface ProductProps {
     product: {
@@ -44,22 +45,27 @@ export default function Product({ product }: ProductProps) {
     }
 
     return (
-        <ProductContainer>
-            <ImageContainer>
-                <Image src={product.imageUrl} width={520} height={480} alt='' />
-            </ImageContainer>
+        <>
+            <Head>
+                <title>{product.name} | 04 shop</title>
+            </Head>
+            <ProductContainer>
+                <ImageContainer>
+                    <Image src={product.imageUrl} width={520} height={480} alt='' />
+                </ImageContainer>
 
-            <ProductDetails>
-                <h1>{product.name}</h1>
-                <span>{product.price}</span>
+                <ProductDetails>
+                    <h1>{product.name}</h1>
+                    <span>{product.price}</span>
 
-                <p>{product.description}</p>
+                    <p>{product.description}</p>
 
-                <button disabled={isCreationCheckoutSession} onClick={handleBuyProduct}>
-                    Comprar agora
-                </button>
-            </ProductDetails>
-        </ProductContainer>
+                    <button disabled={isCreationCheckoutSession} onClick={handleBuyProduct}>
+                        Comprar agora
+                    </button>
+                </ProductDetails>
+            </ProductContainer>
+        </>
     )
 }
 
